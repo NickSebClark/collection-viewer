3e8995e46dbb1f4dd0ac04bdda3800ca862b673b
_MircoWave_ <clarknick1990@gmail.com>
2024-04-29 22:02:00 +0100

    Json editor new stuff
    Started thinking about this again.

--- collection_viewer/main.py
+++ collection_viewer/main.py
@@ -18,4 +18,16 @@ ui.link("Table Display", table, new_tab=True)
 
 # ui.set_page_icon("https://nicegui.io/assets/logo.png")
 
+# read from the test_data json file
+import json
+
+with open("tests/test_data.json", "r") as f:
+    data = json.load(f)[0]
+
+ui.json_editor(
+    {"content": {"json": data}},
+    on_select=lambda e: ui.notify(f"Select: {e}"),
+    on_change=lambda e: ui.notify(f"Change: {e}"),
+)
+
 ui.run(port=8081, title="Collection Viewer", favicon="📚")
